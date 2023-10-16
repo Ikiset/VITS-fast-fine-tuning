f7537cbc5b1f7f575f04c3f69a564addddf61f69
“Ikiset” <daijulien99@gmail.com>
2023-10-16 19:52:33 +0200

    remove no used

--- preprocess.py
+++ preprocess.py
@@ -7,7 +7,6 @@ sys.setrecursionlimit(500000)
 
 
 def preprocess(add_auxiliary_data: bool):
-    langs = ["FR"]
     if os.path.exists("short_character_anno.txt"):
         with open("short_character_anno.txt", 'r', encoding='utf-8') as f:
             short_character_anno = f.readlines()
@@ -45,8 +44,7 @@ def preprocess(add_auxiliary_data: bool):
         path, speaker, txt = line.split("|")
         if len(txt) > 150:
             continue
-        cleaned_text = text._clean_text(txt.replace(
-            "[FR]", ""), hps['data']['text_cleaners'])
+        cleaned_text = text._clean_text(txt, hps['data']['text_cleaners'])
         cleaned_text += "\n" if not cleaned_text.endswith("\n") else ""
         cleaned_new_annos.append(
             path + "|" + str(speaker2id[speaker]) + "|" + cleaned_text)
@@ -101,112 +99,5 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--add_auxiliary_data", type=bool,
                         help="Whether to add extra data as fine-tuning helper")
-    parser.add_argument("--languages", default="FR")
     args = parser.parse_args()
-    if args.languages == "FR":
-        langs = ["[FR]"]
-    new_annos = []
-    # Source 1: transcribed short audios
-    if os.path.exists("short_character_anno.txt"):
-        with open("short_character_anno.txt", 'r', encoding='utf-8') as f:
-            short_character_anno = f.readlines()
-            new_annos += short_character_anno
-    # Source 2: transcribed long audio segments
-    if os.path.exists("./long_character_anno.txt"):
-        with open("./long_character_anno.txt", 'r', encoding='utf-8') as f:
-            long_character_anno = f.readlines()
-            new_annos += long_character_anno
-
-    # Get all speaker names
-    speakers = []
-    for line in new_annos:
-        path, speaker, text = line.split("|")
-        if speaker not in speakers:
-            speakers.append(speaker)
-    assert (len(speakers) !=
-            0), "No audio file found. Please check your uploaded file structure."
-    # Source 3 (Optional): sampled audios as extra training helpers
-    # STEP 1: modify config file
-    with open("./configs/finetune_speaker.json", 'r', encoding='utf-8') as f:
-        hps = json.load(f)
-
-    # assign ids to new speakers
-    speaker2id = {}
-    for i, speaker in enumerate(speakers):
-        speaker2id[speaker] = i
-    # modify n_speakers
-    hps['data']["n_speakers"] = len(speakers)
-    # overwrite speaker names
-    hps['speakers'] = speaker2id
-    hps['train']['log_interval'] = 10
-    hps['train']['eval_interval'] = 100
-    hps['train']['batch_size'] = 16
-    hps['data']['training_files'] = "final_annotation_train.txt"
-    hps['data']['validation_files'] = "final_annotation_val.txt"
-
-    # STEP 2: clean annotations, replace speaker names with assigned speaker IDs
-    import text
-    cleaned_new_annos = []
-    for i, line in enumerate(new_annos):
-        path, speaker, txt = line.split("|")
-        if len(txt) > 150:
-            continue
-        cleaned_text = text._clean_text(txt.replace(
-            "[FR]", ""), hps['data']['text_cleaners'])
-        cleaned_text += "\n" if not cleaned_text.endswith("\n") else ""
-        cleaned_new_annos.append(
-            path + "|" + str(speaker2id[speaker]) + "|" + cleaned_text)
-
-    final_annos = cleaned_new_annos
-
-    if args.add_auxiliary_data:
-        with open("./SIWIS.txt", 'r', encoding='utf-8') as f:
-            old_annos = f.readlines()
-        filtered_old_annos = []
-        for line in old_annos:
-            filtered_old_annos.append(line)
-        old_annos = filtered_old_annos
-        for line in old_annos:
-            path, text = line.split("|")
-            if "SIWIS" not in speakers:
-                speakers.append("SIWIS")
-        num_old_voices = len(old_annos)
-        num_new_voices = len(new_annos)
-        # STEP 1: balance number of new & old voices
-        cc_duplicate = num_old_voices // num_new_voices
-        if cc_duplicate == 0:
-            cc_duplicate = 1
-
-        for i, speaker in enumerate(speakers):
-            speaker2id[speaker] = i
-        # modify n_speakers
-        hps['data']["n_speakers"] = len(speakers)
-        # overwrite speaker names
-        hps['speakers'] = speaker2id
-
-        # STEP 3: clean annotations, replace speaker names with assigned speaker IDs
-        import text
-        cleaned_old_annos = []
-        for i, line in enumerate(old_annos):
-            path, txt = line.split("|")
-            if len(txt) > 150:
-                continue
-            cleaned_text = text._clean_text(txt, hps['data']['text_cleaners'])
-            cleaned_text += "\n" if not cleaned_text.endswith("\n") else ""
-            cleaned_old_annos.append(
-                path + "|" + str(speaker2id["SIWIS"]) + "|" + cleaned_text)
-        # merge with old annotation
-        final_annos = cleaned_old_annos + cc_duplicate * final_annos
-
-    # save modified config
-    with open("./configs/finetune_speaker.json", 'w', encoding='utf-8') as f:
-        json.dump(hps, f, indent=2)
-    # save annotation file
-    with open("./final_annotation_train.txt", 'w', encoding='utf-8') as f:
-        for line in final_annos:
-            f.write(line)
-    # save annotation file for validation
-    with open("./final_annotation_val.txt", 'w', encoding='utf-8') as f:
-        for line in cleaned_new_annos:
-            f.write(line)
-    print("finished")
+    preprocess(args.add_auxiliary_data)
--- scripts/short_audio_transcribe.py
+++ scripts/short_audio_transcribe.py
@@ -5,9 +5,7 @@ import torchaudio
 import argparse
 import torch
 
-lang2token = {
-            'fr': ""
-        }
+
 def transcribe_one(audio_path):
     # load audio and pad/trim it to fit 30 seconds
     audio = whisper.load_audio(audio_path)
@@ -28,12 +26,14 @@ def transcribe_one(audio_path):
     print(result.text)
     return lang, result.text
 
+
 def short_audio_load(i, parent_dir, speaker, wavfile, target_sr, to_long_file, to_long_call):
     wav, sr = torchaudio.load(parent_dir + speaker + "/" + wavfile, frame_offset=0, num_frames=-1, normalize=True,
                               channels_first=True)
     wav = wav.mean(dim=0).unsqueeze(0)
     if sr != target_sr:
-        wav = torchaudio.transforms.Resample(orig_freq=sr, new_freq=target_sr)(wav)
+        wav = torchaudio.transforms.Resample(
+            orig_freq=sr, new_freq=target_sr)(wav)
     if wav.shape[1] / sr > 20:
         to_long_file.append(wavfile)
         to_long_call = f"{', '.join(to_long_file)} too long, ignoring\n"
@@ -42,9 +42,10 @@ def short_audio_load(i, parent_dir, speaker, wavfile, target_sr, to_long_file, t
     torchaudio.save(save_path, wav, target_sr, channels_first=True)
     return save_path, to_long_call
 
+
 def short_audio_transcribe(whisper_size="small"):
-    lang2token = {'fr': "[FR]"}
-    assert (torch.cuda.is_available()), "Please enable GPU in order to run Whisper!"
+    assert (torch.cuda.is_available()
+            ), "Please enable GPU in order to run Whisper!"
     model = whisper.load_model(whisper_size)
     parent_dir = "./custom_character_voice/"
     speaker_names = list(os.walk(parent_dir))[0][1]
@@ -58,7 +59,7 @@ def short_audio_transcribe(whisper_size="small"):
     to_long_call = ""
     lang_error_file = []
     lang_error_call = ""
-    
+
     for speaker in speaker_names:
         for i, wavfile in enumerate(list(os.walk(parent_dir + speaker))[0][2]):
             # try to load file as audio
@@ -71,13 +72,13 @@ def short_audio_transcribe(whisper_size="small"):
                     continue
                 # transcribe text
                 lang, text = transcribe_one(save_path)
-                if lang not in list(lang2token.keys()):
+                if lang != 'fr':
                     lang_error_file.append(wavfile+f"({lang})")
-                    lang_error_call = f"{', '.join(lang_error_file)} : not suported (filename lang)"
+                    lang_error_call = f"{', '.join(lang_error_file)} : is not french"
                     continue
-                text = lang2token[lang] + text + lang2token[lang] + "\n"
+                text = text + "\n"
                 speaker_annos.append(save_path + "|" + speaker + "|" + text)
-                
+
                 processed_files += 1
                 yield(processed_files, total_files, to_long_call, lang_error_call)
             except:
@@ -87,16 +88,13 @@ def short_audio_transcribe(whisper_size="small"):
             f.write(line)
     yield "finished"
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--languages", default="FR")
     parser.add_argument("--whisper_size", default="small")
     args = parser.parse_args()
-    if args.languages == "FR":
-        lang2token = {
-            'fr': "[FR]"
-        }
-    assert (torch.cuda.is_available()), "Please enable GPU in order to run Whisper!"
+    assert (torch.cuda.is_available()
+            ), "Please enable GPU in order to run Whisper!"
     model = whisper.load_model(args.whisper_size)
     parent_dir = "./custom_character_voice/"
     speaker_names = list(os.walk(parent_dir))[0][1]
@@ -118,25 +116,25 @@ if __name__ == "__main__":
                                           channels_first=True)
                 wav = wav.mean(dim=0).unsqueeze(0)
                 if sr != target_sr:
-                    wav = torchaudio.transforms.Resample(orig_freq=sr, new_freq=target_sr)(wav)
+                    wav = torchaudio.transforms.Resample(
+                        orig_freq=sr, new_freq=target_sr)(wav)
                 if wav.shape[1] / sr > 20:
                     print(f"{wavfile} too long, ignoring\n")
                 save_path = parent_dir + speaker + "/" + f"processed_{i}.wav"
                 torchaudio.save(save_path, wav, target_sr, channels_first=True)
                 # transcribe text
                 lang, text = transcribe_one(save_path)
-                if lang not in list(lang2token.keys()):
-                    print(f"{lang} not supported, ignoring\n")
+                if lang != 'fr':
+                    print(f"{lang} is not french, ignoring\n")
                     continue
-                text = lang2token[lang] + text + lang2token[lang] + "\n"
+                text = text + "\n"
                 speaker_annos.append(save_path + "|" + speaker + "|" + text)
-                
+
                 processed_files += 1
                 print(f"Processed: {processed_files}/{total_files}")
             except:
                 continue
 
- 
     if len(speaker_annos) == 0:
         print("Warning: no short audios found, this IS expected if you have only uploaded long audios, videos or video links.")
         print("this IS NOT expected if you have uploaded a zip file of short audios. Please check your file structure or make sure your audio language is supported.")
